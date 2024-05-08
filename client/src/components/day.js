58b0ba19a66abbd243f1604cddfb18c4a7a67f59
devora6936 <devora6936@gmail.com>
2024-05-08 15:15:45 +0300

    cange lines order

--- client/src/components/day.js
+++ client/src/components/day.js
@@ -8,28 +8,26 @@ import { Dialog } from "primereact/dialog";
 
 
 const Day = (props) => {
-    const [heb, setHeb] = useState()
-    const [visible, setVisible] = useState(false)
-    const { data: events, isLoading, isError, error } = useGetEventsByDAteQuery(date.split('T')[0])
-
     let date = new Date(props.year, props.month, props.day)
     const hebDate = date.getDay()
-    date = date.toISOString()
-    const comp = new Date(props.date.getFullYear(), props.date.getMonth(), props.date.getDate() + 1).toISOString().split('T')[0]
-
+    date = date.toISOString()   
+    const comp=new Date(props.date.getFullYear(),props.date.getMonth(),props.date.getDate()+1).toISOString().split('T')[0]
+    const [heb, setHeb] = useState()
+    const { data: events, isLoading, isError, error } = useGetEventsByDAteQuery(date.split('T')[0])
+    const [visible,setVisible]=useState(false)
     //הוספה
-    const [morning, setMorning] = useState(false)
-    const [night, setNight] = useState(false)
-    const [shabbat, setShabbat] = useState(false)
-    const [kidush, setKidush] = useState(false)
-    const [day, setDay] = useState(false)
+    const [morning,setMorning]=useState(false)
+    const [night,setNight]=useState(false)
+    const [shabbat,setShabbat]=useState(false)
+    const [kidush,setKidush]=useState(false)
+    const [day,setDay]=useState(false)
 
     //עדכון ומחיקה
-    const [morningflag, setMorningFlag] = useState(false)
-    const [nightflag, setNightFlag] = useState(false)
-    const [shabbatFlag, setShabbatFlag] = useState(false)
-    const [dayFlag, setDayFlag] = useState(false)
-    const [kidushFlag, setKIdushFlag] = useState(false)
+    const [morningflag,setMorningFlag]=useState(false)
+    const [nightflag,setNightFlag]=useState(false)
+    const [shabbatFlag,setShabbatFlag]=useState(false)
+    const [dayFlag,setDayFlag]=useState(false)
+    const [kidushFlag,setKIdushFlag]=useState(false)
 
     useEffect(() => {
         axios.get(`https://www.hebcal.com/converter?cfg=json&date=${date}&g2h=1&strict=1`).then((response) => {
@@ -37,16 +35,15 @@ const Day = (props) => {
         });
     }, [date]);
 
-    const findEventInDay = (type) => {
-        return events?.filter((e) => e.eventType == type)[0]
+    const findEventInDay=(type)=>{
+        return events?.filter((e)=>e.eventType==type)[0]
     }
-
     const renderButtons = () => {
         if (props.isSmallScreen) {
             return (
                 <div className="buttons-container">
                     <Button label="אירועים" severity="אירועים" text onClick={() => setVisible(true)} />
-                    <Dialog header={`${heb?.heDateParts.d} ${heb?.heDateParts.m}`} visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)} >
+                    <Dialog header={`${heb?.heDateParts.d} ${heb?.heDateParts.m}`}  visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)} >
                         <div className="buttons-container">
                             {heb ? hebDate == 0 ?
                                 <div>
@@ -83,33 +80,32 @@ const Day = (props) => {
             );
         }
     };
-
     return (
         <>
-            <div className="ourday" style={{ border: date.split('T')[0] == comp ? '2px solid #8284f5' : '1px solid #dcdfe1' }}>
+            <div className="ourday" style={{ border:date.split('T')[0]==comp?'2px solid #8284f5':'1px solid #dcdfe1'}}>
                 <div>
                     <div>{heb && `${heb.heDateParts.d} ${heb.heDateParts.m} `}</div>
-                    {heb ? hebDate == 0 ? heb.events[heb.events.length - 1].includes("Omer") ? <div>{heb.events.slice(0, heb.events.length - 1)}</div> : <div>{heb.events}</div> : <div>{heb.events.slice(0, heb.events.length - 2)}</div> : <></>}
+                    {heb ? hebDate == 0 ?heb.events[heb.events.length - 1].includes("Omer")?<div>{heb.events.slice(0, heb.events.length - 1)}</div>:<div>{heb.events}</div> : <div>{heb.events.slice(0, heb.events.length - 2)}</div> : <></>}
                 </div>
                 <div className="footer">
                     {renderButtons()}
                 </div>
             </div>
-
-            {morning ? <AddEventDialog eventType={'בוקר'} date={date} visible={true} setVisible={setMorning} heb={heb} /> : <></>}
-            {morningflag ? <SetEventDialog eventType={'בוקר'} date={date} setVisible={setMorningFlag} visible={true} event={events?.filter((e) => e.eventType == 'בוקר')[0]} /> : <></>}
-
-            {night ? <AddEventDialog eventType={'ערב'} date={date} visible={true} setVisible={setNight} heb={heb} /> : <></>}
-            {nightflag ? <SetEventDialog eventType={'ערב'} date={date} setVisible={setNightFlag} visible={true} event={events?.filter((e) => e.eventType == 'ערב')[0]} /> : <></>}
-
-            {shabbat ? <AddEventDialog eventType={'שבת'} date={date} visible={true} setVisible={setShabbat} heb={heb} /> : <></>}
-            {shabbatFlag ? <SetEventDialog eventType={'שבת'} date={date} setVisible={setShabbatFlag} visible={true} event={events?.filter((e) => e.eventType == 'שבת')[0]} /> : <></>}
-
-            {kidush ? <AddEventDialog eventType={'קידוש'} date={date} visible={true} setVisible={setKidush} heb={heb} /> : <></>}
-            {kidushFlag ? <SetEventDialog eventType={'קידוש'} date={date} setVisible={setKIdushFlag} visible={true} event={events?.filter((e) => e.eventType == 'קידוש')[0]} /> : <></>}
-
-            {day ? <AddEventDialog eventType={'יום שלם'} date={date} visible={true} setVisible={setDay} heb={heb} /> : <></>}
-            {dayFlag ? <SetEventDialog eventType={'יום שלם'} date={date} setVisible={setDayFlag} visible={true} event={events?.filter((e) => e.eventType == 'יום שלם')[0]} /> : <></>}
+          
+            {morning?<AddEventDialog eventType={'בוקר'} date={date} visible={true} setVisible={setMorning} heb={heb}/>:<></>}
+            {morningflag?<SetEventDialog eventType={'בוקר'} date={date} setVisible={setMorningFlag}visible={true}  event={events?.filter((e)=>e.eventType=='בוקר')[0]}/>:<></>}
+            
+            {night?<AddEventDialog eventType={'ערב'} date={date} visible={true} setVisible={setNight} heb={heb}/>:<></>}
+            {nightflag?<SetEventDialog eventType={'ערב'} date={date} setVisible={setNightFlag}visible={true}  event={events?.filter((e)=>e.eventType=='ערב')[0]}/>:<></>}
+            
+            {shabbat?<AddEventDialog eventType={'שבת'} date={date} visible={true} setVisible={setShabbat} heb={heb}/>:<></>}
+            {shabbatFlag?<SetEventDialog eventType={'שבת'} date={date} setVisible={setShabbatFlag}visible={true}  event={events?.filter((e)=>e.eventType=='שבת')[0]}/>:<></>}
+            
+            {kidush?<AddEventDialog eventType={'קידוש'} date={date} visible={true} setVisible={setKidush} heb={heb}/>:<></>}
+            {kidushFlag?<SetEventDialog eventType={'קידוש'} date={date} setVisible={setKIdushFlag}visible={true}  event={events?.filter((e)=>e.eventType=='קידוש')[0]}/>:<></>}
+           
+            {day?<AddEventDialog eventType={'יום שלם'} date={date} visible={true} setVisible={setDay} heb={heb}/>:<></>}
+            {dayFlag?<SetEventDialog eventType={'יום שלם'} date={date} setVisible={setDayFlag}visible={true}  event={events?.filter((e)=>e.eventType=='יום שלם')[0]}/>:<></>}
 
         </>
     )
