920750a797816eb949b641b2509bd54ee118b9b7
devora6936 <devora6936@gmail.com>
2024-05-14 20:47:46 +0300

    tring to fix bug

--- client/src/components/pages/events.js
+++ client/src/components/pages/events.js
@@ -4,6 +4,7 @@ import { Divider } from 'primereact/divider';
 import Datepicker from "./datePicker";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { ReactJewishDatePicker } from "react-jewish-datepicker";
 
 
 const Events = () => {
@@ -15,7 +16,14 @@ const Events = () => {
             {isUserLoggedIn &&
                 <div className="flex align-items-center">
                     <Card className="cal">
-                        <Datepicker date={date} setDate={setDate1} />
+                        {/* <Datepicker date={date} setDate={setDate1} /> */}
+                        <ReactJewishDatePicker
+                            value={date}
+                            isHebrew
+                            onClick={(day) => {
+                                setDate1(day.date);
+                            }}
+                        />
                         <Divider />
                         <Calendar date={date} />
                     </Card>
